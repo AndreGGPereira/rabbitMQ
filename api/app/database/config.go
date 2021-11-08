088a4ee5531489de45c99e796897b41c815f6030
Andre <andreggp@gmail.com>
2021-11-08 05:57:53 -0300

    config

--- api/app/database/config.go
+++ api/app/database/config.go
@@ -2,21 +2,11 @@ package database
 
 import "fmt"
 
-/* var (
+var (
 	dbUsername = "postgres"
 	dbHost     = "db"
 	dbPassword = "postgres"
 	dbname     = "nuveo"
 	pgConnStr  = fmt.Sprintf("host=%s dbname=%s user=%s password=%s sslmode=disable",
 		dbHost, dbname, dbUsername, dbPassword)
-) */
-
-var (
-	dbUsername = "postgres"
-	dbHost     = "localhost"
-	dbPassword = "andre110407"
-	dbname     = "nuveo"
-	dbport     = "5432"
-	pgConnStr  = fmt.Sprintf("host=%s dbname=%s port=%s user=%s password=%s sslmode=disable",
-		dbHost, dbname, dbport, dbUsername, dbPassword)
 )
